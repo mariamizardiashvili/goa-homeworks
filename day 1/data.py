5430c3b4d67d7e423a08a185ffaf5e09bb84e036
mariamizardiashvili <zardiashvilimariami7@gmail.com>
2024-02-28 10:19:01 -0800

    commit

---
+++ day 1/data.py
@@ -0,0 +1,5 @@
+name = 'mariami'
+age = 15 
+height = 170
+
+print("chemi saxelia", name, ",me var", age, "wlis", "da chemi simaglea", height, "santimetri") 
---
+++ day 4/sayty.py
@@ -0,0 +1,5 @@
+name=input ("enter your name")
+surname=input("enter your last name")
+age=input("enter your age")
+
+print("your name" + name + " " + surname + " and you are " + "+ age + " "abot age " ) 
