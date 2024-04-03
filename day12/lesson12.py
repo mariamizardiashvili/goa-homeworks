8332a93dc291955a06d1f13fdf77d1ca9d38bd9f
mariamizardiashvili <161319000+mariamizardiashvili@users.noreply.github.com>
2024-04-03 20:41:03 +0400

    Add files via upload

---
+++ day12/lesson12.py
@@ -0,0 +1,9 @@
+num=int(input("please enter number:"))
+
+if num==0:
+    print("0")
+elif num > 0:
+    print("მეტია ნულზე")
+elif num < 0:
+    print  ("ნაკლებია ნულზე")
+        
---
+++ day13/lesson13.py
@@ -0,0 +1,7 @@
+num= int (input( "enter yout age"))
+if num > 18:
+    print("shen 18 wlis xar" )
+elif  num >= 18 < 60:
+    print("sashualo wlovanebis xar" )
+elif num > 60:
+    print("shen xar xanshi shesuli")        
