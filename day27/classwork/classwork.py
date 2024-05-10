2807f1a24210346fd8535fe83fbf404b2e497556
mariamizardiashvili <zardiashvilimariami7@gmail.com>
2024-05-10 11:29:22 +0400

    commit

---
+++ day27/classwork/classwork.py
@@ -0,0 +1,31 @@
+# names = ["ani,nini,nanuka,mariami,nino"]
+# ages = [14,13,15,12,15]
+
+#    print(names)
+
+
+
+
+# information_about_me = ["nino xvichia," "15 wlis", "swavlobs goashi"]
+
+# print(information_about_me)
+
+# nim1 = input("swavlob goashi? :")
+
+# while num1 != "diax":
+#     num1 = input("swavlob goashi? :")
+# print("true")    
+
+
+
+# films= ["avatar,home alone,nikolas..."]
+# print(films)
+# print(films[1])
+# num1_5 = [1,2,3]
+# num5_10 = [5,6,7,8,9,10]
+# print(num1_5 + num5_10)
+# text = "mar"
+# print(text[1])
+# text = "mariami"
+# array = [1,2,3,4,5,6,7,8]
+# print(len(array))
