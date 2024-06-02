7b8e8b687519ccd5bc181599d0198fe3829bd02c
mariamizardiashvili <zardiashvilimariami7@gmail.com>
2024-06-02 14:25:06 +0400

    commit

---
+++ day09/homework/homework.py
@@ -0,0 +1,5 @@
+print("gamargoba mainteresebs romeli mshobeli upro didia deda tu mama?")
+user_age1 = input("ramdeni wlisaa dedaseni? ")
+user_age2 = input("ramdeni wlisaa mamaseni? ")
+
+print("tu iqneba fols esegi dedaseni ufro didia da tu true esego mamaseni")
---
+++ day34/homework/lesson0.py
@@ -0,0 +1,9 @@
+# შექმნათ სია სადაც ჩაამატებთ თქვენი ყველა ჯგუფელის სახელს და გვარს, შემდეგ კი დაბეჭდავთ შუა 10 მოსწავლეს
+goa_13 = ["andria qorchilava", "nikoloz mirziashvili", "andria shanava", "mate kotorashvili",
+ "saba lomsadze", "gega diasamidze", "dato miribiani", "demetre biganashvili", "nika kuchashvili",
+  "andria alavidze", "andria lakobishvili", "anastasia khvedelidze", "andria samsonidze",
+   "erekle karagoziashvili", "gabriel gvinashvili", "gio alada", "giviko kavrisvili",
+    "luxumi zurabiani", "masho lomidze", "mate tolordava", "sandro chapidze",
+ "tazo kldiashvili", "vika gvilia", "bibilashvili giorgi", "giorgi qareli", "mishiko berelidze"]
+
+print(goa_13[5:15])
