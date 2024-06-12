423ffa693c2cbb083846a66f343c6a9de833e144
mariamizardiashvili <zardiashvilimariami7@gmail.com>
2024-06-12 17:47:03 +0400

    commit

---
+++ day38/homework/lesson.py
@@ -0,0 +1,58 @@
+# 1) # შექმნათ სია სადაც მოათავსებთ თქვენი საყავრელ ფილმების სახელებს(მინიმუმ 10 სახელი), შემდეგ კი მათ დაბეჭდავთ ცალცალკე, 
+films= ["avatar,home alone,nikolas..."]
+# print(films)
+# print(films[1])
+# num1_5 = [1,2,3]
+# num5_10 = [5,6,7,8,9,10]
+# print(num1_5 + num5_10)
+# text = "mar"
+# print(text[1])
+# text = "mariami"
+# array = [1,2,3,4,5,6,7,8]
+# print(len(array))
+
+
+
+
+
+
+
+
+
+
+
+#   2) # შექმნათ სია სადაც მოათავსებთ თქვენი ოჯახის წევრების სახელებს(მინიმუმ 10 სახელი) და შემდეგ კი დაბეჭდავთ გრძელ წინადადებაში ყველა სახელს ერთად მაგ. ჩემი სახელია {თქვენი სახელი} ჩემი დედიკოს სახელია {დედიკოს სახელი} ჩემი მამიკოს სახელია {მამიკოს სახელს}
+
+
+
+
+
+
+
+
+
+    # 3) # შექმნათ სია სადაც მოათავსებთ თქვენი მეგობრის სახელებს(მინიმუმ 10 სახელი) შემდეგ დაბეჭდავთ მათ ცალცალკე for ლუპის გამოყენებით
+# names = ["ani,nini,nanuka,mariami,nino,ani,salome"]
+# ages = [14,13,15,12,15,13,16]
+
+#    print(names)
+
+
+
+
+
+
+
+
+
+
+
+# 4) შექმნით სიას სადაც მოათავსებთ თქვენი ყველა ჯგუფელის სახელს და გვარს, დაბეჭდავთ მათ ცალცალკე და ყველას დაახასიათებთ მაგ. ანდრია არის ასეთი, ასეთი, ასეთი ..
+goa_13 = ["andria qorchilava", "nikoloz mirziashvili", "andria shanava", "mate kotorashvili",
+ "saba lomsadze", "gega diasamidze", "dato miribiani", "demetre biganashvili", "nika kuchashvili",
+  "andria alavidze", "andria lakobishvili", "anastasia khvedelidze", "andria samsonidze",
+   "erekle karagoziashvili", "gabriel gvinashvili", "gio alada", "giviko kavrisvili",
+    "luxumi zurabiani", "masho lomidze", "mate tolordava", "sandro chapidze",
+ "tazo kldiashvili", "vika gvilia", "bibilashvili giorgi", "giorgi qareli", "mishiko berelidze"]
+
+print(goa_13[5:15])
